d924a6dd5f1ca4f01bed14c36eadf6c0e94a2eff
Boris Lechev <b.lechev@onebitsoftware.net>
2021-09-22 16:35:54 +0300

    Add Header component in ToDo App

--- ToDo-App/App.js
+++ ToDo-App/App.js
@@ -1,13 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { FlatList, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import Header from './components/header';
 
 export default function App() {
+  const [todos, setTodos] = useState([
+    {text: 'finish this tutorial', key: '1'},
+    {text: 'make the I app I want to do', key: '2'},
+    {text: 'find a job', key: '3'},
+    {text: 'get money', key: '4'},
+  ]);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!!</Text>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.content}>
+        <Header />
+        <View style={styles.list}>
+          <FlatList
+            data={todos}
+            renderItem={({ item }) => (
+              <Text>{item.text}</Text>
+            )}
+          />
+        </View>
+      </View>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -15,7 +33,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });

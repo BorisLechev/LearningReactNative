7e96444624790e1717900785b3850c73406fe91c
BorisLechev <b.k.lechev@gmail.com>
2021-09-23 10:45:44 +0300

    Add form validation and dismiss keyboard

--- ToDo-App/App.js
+++ ToDo-App/App.js
@@ -1,16 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { FlatList, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { FlatList, SafeAreaView, StyleSheet, View, Alert, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import AddToDo from './components/addToDo';
 import Header from './components/header';
 import ToDoItem from './components/toDoItem';
 
 export default function App() {
   const [todos, setTodos] = useState([
-    {text: 'finish this tutorial', key: '1'},
-    {text: 'make the I app I want to do', key: '2'},
-    {text: 'find a job', key: '3'},
-    {text: 'get money', key: '4'},
+    { text: 'finish this tutorial', key: '1' },
+    { text: 'make the I app I want to do', key: '2' },
+    { text: 'find a job', key: '3' },
+    { text: 'get money', key: '4' },
   ]);
 
   onPress = (key) => {
@@ -20,30 +20,38 @@ export default function App() {
   }
 
   onSubmit = (text) => {
-    setTodos((prevTodos) => {
-      return [
-        { text: text, key: Math.random().toString() },
-        ...prevTodos,
-      ];
-    });
+    if (text.length >= 3) {
+      setTodos((prevTodos) => {
+        return [
+          { text: text, key: Math.random().toString() },
+          ...prevTodos,
+        ];
+      });
+    } else {
+      Alert.alert("OOPS!", "ToDos should be at least 3 characters long."), [
+        { text: "OK", onPress: () => console.log("OK Pressed") }
+      ]
+    }
   }
 
   return (
-    <SafeAreaView style={styles.container}>
-      <Header />
-      <View style={styles.content}>
-        <AddToDo onSubmit={this.onSubmit} />
-        <View style={styles.list}>
-          <FlatList
-            data={todos}
-            renderItem={({ item }) => (
-              <ToDoItem item={item} onPress={this.onPress} />
-            )}
-          />
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <SafeAreaView style={styles.container}>
+        <Header />
+        <View style={styles.content}>
+          <AddToDo onSubmit={this.onSubmit} />
+          <View style={styles.list}>
+            <FlatList
+              data={todos}
+              renderItem={({ item }) => (
+                <ToDoItem item={item} onPress={this.onPress} />
+              )}
+            />
+          </View>
         </View>
-      </View>
-      <StatusBar style="auto" />
-    </SafeAreaView>
+        <StatusBar style="auto" />
+      </SafeAreaView>
+    </TouchableWithoutFeedback>
   );
 }
 
